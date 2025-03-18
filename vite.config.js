ce0321d20bc195d29a258c4db3c299967a4cd8f2
Nanthawat45 <Nanthawat.ohm45@gmail.com>
2025-03-18 15:55:21 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/My-bmi-project/",
-  plugins: [react(), tailwindcss()],
+  base: "/my-bmi-project/",
+  plugins: [react()],
 });
